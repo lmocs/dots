b86b79cd59494531ca22c0afc2bd3bcf6b57d91d
lmocs <loganmoreno.cs@gmail.com>
2025-08-28 15:32:06 -0700

    Enable autocmds.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,7 @@
 -- If there is no `init.lua` in a folder, direct module calls must be made
+require("core.autocmds")
 require("core.keymaps")
+
 require("config.lazy")
 
 -- `plugins/` has an `init.lua`, so calling just the directory is sufficient
